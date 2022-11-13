5a8cb4d482b63b775d53d3ba55ee38b1156e3bfc
alvinbengeorge <alvinallen333@gmail.com>
2022-11-13 15:51:21 +0530

    Using CRUD more

--- Server/movie/index.cjs
+++ Server/movie/index.cjs
@@ -11,8 +11,13 @@ let data = JSON.parse(fs.readFileSync(path.resolve(__dirname, "Movies.json")));
 function saveToFile() {
     fs.writeFileSync(path.resolve(__dirname, "Movies.json"), JSON.stringify(data, null, "\t"));
 }
+
 app.get("/movies", (req, res) => {
-    res.send(data);
+    outputData = {};
+    for (let i of data) {
+        outputData[i.id] = i.title
+    }
+    res.send(outputData);
 });
 
 app.get("/movies/:id", (req, res) => {
@@ -47,5 +52,34 @@ app.put("/downvote/:id", (req, res) => {
     }
 });
 
+app.delete("/delete/:id", (req, res) => {
+    let id = req.params.id;
+    if (id in data) {        
+        if (req.headers.token === admin.adminToken) {
+            delete data[id];
+            res.send({ "message": "Movie deleted" });
+            saveToFile();
+        } else {
+            res.send({ "message": "You are not authorized to delete this movie" });
+        }
+    } else {
+        res.send({ "message": "This movie does not exist" });
+    }
+});
+
+app.patch("/update/:id", (req, res) => {
+    let id = req.params.id;
+    if (id in data) {
+        if (req.headers.token === admin.adminToken) {
+            data[id] = req.body;
+            res.send(data[id]);
+            saveToFile();
+        } else {
+            res.send({ "message": "You are not authorized to edit this movie" });
+        }
+    } else {
+        res.send({ "message": "This movie does not exist" });
+    }
+});
 
 app.listen(8080);
